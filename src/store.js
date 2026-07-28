396a4531cef7baed3f3ec24d2dabf768401a0d4d
Eike Hensler <eike.hensler@gmail.com>
2026-07-28 16:48:09 +0200

    fix: remove duplicate size() method in CounterStore

--- src/store.js
+++ src/store.js
@@ -53,15 +53,6 @@ export class CounterStore {
   }
 
   /**
-   * Return the number of unique counter names in the store.
-   *
-   * @returns {number} Count of unique counter names.
-   */
-  size() {
-    return this.#counters.size;
-  }
-
-  /**
    * Return all counters sorted by name.
    *
    * @returns {{ name: string, value: number }[]} Sorted array of counter entries.
